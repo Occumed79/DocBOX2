87406548b96c895c148062137ebcd5b1135d7eeb
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 07:54:42 -0700

    Wire the cinematic handoff into the provider experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,10 +1,12 @@
 import CinematicContinuity from '@/components/experience/CinematicContinuity';
+import CinematicHandoff from '@/components/experience/CinematicHandoff';
 import ProviderExperience from '@/components/experience/ProviderExperience';
 
 export default function ProviderExperiencePage() {
   return (
     <>
       <CinematicContinuity />
+      <CinematicHandoff />
       <ProviderExperience />
     </>
   );
