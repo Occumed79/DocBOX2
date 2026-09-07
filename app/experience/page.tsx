b503cd34157499efa71a3392cafdeb0e34ed2dea
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 12:03:31 -0700

    Carry one narrative thread through the cinematic middle

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,5 +1,6 @@
 import CinematicContinuity from '@/components/experience/CinematicContinuity';
 import CinematicHandoff from '@/components/experience/CinematicHandoff';
+import NarrativeThread from '@/components/experience/NarrativeThread';
 import ProviderCompass from '@/components/experience/ProviderCompass';
 import ProviderExperience from '@/components/experience/ProviderExperience';
 import polish from './ProviderStaticPolish.module.css';
@@ -8,6 +9,7 @@ export default function ProviderExperiencePage() {
   return (
     <>
       <CinematicContinuity />
+      <NarrativeThread />
       <CinematicHandoff />
       <ProviderCompass />
       <div className={polish.surface}>
