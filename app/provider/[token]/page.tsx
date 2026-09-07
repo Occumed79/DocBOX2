eb5b47767fd29c784e5c886ac084ecdd8f9ab044
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 13:43:13 -0700

    Render secure Forms invitation directly at provider token route

--- app/provider/[token]/page.tsx
+++ app/provider/[token]/page.tsx
@@ -1,8 +1,8 @@
-import { redirect } from 'next/navigation';
+import FormsProviderInvitation from '@/components/forms/FormsProviderInvitation';
 
 type Props = { params: Promise<{ token: string }> };
 
 export default async function ProviderInvitationEntry({ params }: Props) {
   const { token } = await params;
-  redirect(`/experience?provider_token=${encodeURIComponent(token)}#provider-details`);
+  return <FormsProviderInvitation token={token} />;
 }
