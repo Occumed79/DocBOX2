a2b9c1f1b21f8e35f6fd9baf4d668c3fd152b179
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 01:05:36 -0700

    Mount continuous story controller

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,5 +1,11 @@
+import CinematicContinuity from '@/components/experience/CinematicContinuity';
 import ProviderExperience from '@/components/experience/ProviderExperience';
 
 export default function ProviderExperiencePage() {
-  return <ProviderExperience />;
+  return (
+    <>
+      <CinematicContinuity />
+      <ProviderExperience />
+    </>
+  );
 }
