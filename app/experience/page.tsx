fe49531e00f025d2d2a97ef27abd546444bec951
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 08:01:30 -0700

    Apply editorial polish to the static provider experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -2,6 +2,7 @@ import CinematicContinuity from '@/components/experience/CinematicContinuity';
 import CinematicHandoff from '@/components/experience/CinematicHandoff';
 import ProviderCompass from '@/components/experience/ProviderCompass';
 import ProviderExperience from '@/components/experience/ProviderExperience';
+import polish from './ProviderStaticPolish.module.css';
 
 export default function ProviderExperiencePage() {
   return (
@@ -9,7 +10,9 @@ export default function ProviderExperiencePage() {
       <CinematicContinuity />
       <CinematicHandoff />
       <ProviderCompass />
-      <ProviderExperience />
+      <div className={polish.surface}>
+        <ProviderExperience />
+      </div>
     </>
   );
 }
