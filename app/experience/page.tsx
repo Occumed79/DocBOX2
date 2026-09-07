fb90ad07879de1f4e8241ddf14c956058fe7db8c
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-07 12:33:58 -0700

    Add provider FAQ to the practical onboarding flow

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,6 +5,7 @@ import DirectorOverlay from '@/components/experience/DirectorOverlay';
 import NarrativeThread from '@/components/experience/NarrativeThread';
 import ProviderCompass from '@/components/experience/ProviderCompass';
 import ProviderExperience from '@/components/experience/ProviderExperience';
+import ProviderFAQ from '@/components/experience/ProviderFAQ';
 import polish from './ProviderStaticPolish.module.css';
 
 export default function ProviderExperiencePage() {
@@ -18,6 +19,7 @@ export default function ProviderExperiencePage() {
       <DirectorOverlay />
       <div className={polish.surface}>
         <ProviderExperience />
+        <ProviderFAQ />
       </div>
     </>
   );
