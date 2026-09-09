419e762832d058019578271e9b6130044c8bd5b4
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-09 04:55:28 -0700

    Mount synchronized geography layer in provider network explorer

--- app/experience/network/page.tsx
+++ app/experience/network/page.tsx
@@ -1,5 +1,11 @@
 import NetworkExperience from '@/components/experience/network/NetworkExperience';
+import NetworkLandLayer from '@/components/experience/network/NetworkLandLayer';
 
 export default function Page() {
-  return <NetworkExperience />;
+  return (
+    <>
+      <NetworkExperience />
+      <NetworkLandLayer />
+    </>
+  );
 }
