be3d44d2905283867c8b73a2bc2b39220b336155
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-10 20:57:26 -0700

    Remove flat land layer from Cesium atlas

--- app/experience/network/page.tsx
+++ app/experience/network/page.tsx
@@ -1,11 +1,5 @@
 import NetworkExperience from '@/components/experience/network/NetworkExperience';
-import NetworkLandLayer from '@/components/experience/network/NetworkLandLayer';
 
 export default function Page() {
-  return (
-    <>
-      <NetworkExperience />
-      <NetworkLandLayer />
-    </>
-  );
+  return <NetworkExperience />;
 }
